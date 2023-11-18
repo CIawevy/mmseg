964a20db215b9cfba17ba7e33a5dde44affcde8e
CIawevy <519511374@qq.com>
2023-11-18 10:44:52 +0800

    基于trufor的localization test 部署的数据集测试config 注意基于9996的数据集路径更新。

--- configs/_base_/datasets/forensics.py
+++ configs/_base_/datasets/forensics.py
@@ -1,5 +1,4 @@
 # dataset settings
-
 dataset_type = 'ForensicsDataset'
 data_root = '/data/ipad/Forgery/'
 crop_size = (512, 512)
@@ -8,14 +7,14 @@ train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', binary=True),
     dict(type='SelfRandomResize', ratio_range=(0.5, 1.5)),
-    #dict(type='Resize', scale=crop_size),
+    # dict(type='Resize', scale=crop_size),
     # dict(
     #     type='RandomResize',
     #     scale=crop_size,
     #     ratio_range=(0.5, 2.0),
     #     keep_ratio=True),
     dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
-    dict(type='RandomFlip', prob=0.5),
+    # dict(type='RandomFlip', prob=0.5),
     # dict(type='PhotoMetricDistortion'),
     dict(type='JPEGcompression', quality_factor=(30, 100)),
     dict(type='PackSegInputs')
@@ -23,8 +22,9 @@ train_pipeline = [
 
 test_pipeline = [
     dict(type='LoadImageFromFile'),
-    # dict(type='Resize', scale=crop_size),
-    # dict(type='SelfRandomResize', ratio_range=(1.5, 2)),
+    # dict(type='Resize', scale=(512,512)),#trufor 测试不需要resize
+    # dict(type='JPEGcompression', quality_factor=(70,71)),
+    # dict(type='SelfRandomResize', ratio_range=(1.0, 1.5)),
     # add loading annotation after ``Resize`` because ground truth
     # does not need to do resize data transform
     dict(type='LoadAnnotations', binary=True),
@@ -33,7 +33,7 @@ test_pipeline = [
 
 CASIAv2_tamp_train_dataset = dict(
     type='RepeatDataset',
-    times=5,
+    times=34,
     dataset=dict(
         type=dataset_type,
         data_root=data_root,
@@ -45,7 +45,7 @@ CASIAv2_tamp_train_dataset = dict(
 )
 CASIAv2_authentic_train_dataset = dict(
     type='RepeatDataset',
-    times=5,
+    times=34,
     dataset=dict(
         type=dataset_type,
         data_root=data_root,
@@ -59,7 +59,7 @@ CASIAv2_authentic_train_dataset = dict(
 
 IMD2020_tamp_train_dataset = dict(
     type='RepeatDataset',
-    times=10,
+    times=160,
     dataset=dict(
         type=dataset_type,
         data_root=data_root,
@@ -71,7 +71,7 @@ IMD2020_tamp_train_dataset = dict(
 )
 IMD2020_authentic_train_dataset = dict(
     type='RepeatDataset',
-    times=10,
+    times=160,
     dataset=dict(
         type=dataset_type,
         data_root=data_root,
@@ -193,18 +193,22 @@ CASIAv1_plus_all_test_dataset=dict(
 
 
 
-Columbia_authentic_test_dataset=dict(
+Columbia_authentic_test_dataset = dict(
     type=dataset_type,
-    data_root="/data/ipad/Forgery/test/Columbia/AU/",
+    data_root="/data/ipad/Forgery/pristine/Columbia/AU/",
+    img_suffix = '.tif',
+    seg_map_suffix='.png',
     global_class=0,
     data_prefix=dict(
         img_path="image",
         seg_map_path="GT"),
     pipeline=test_pipeline)
 
-Columbia_tamp_test_dataset=dict(
+Columbia_tamp_test_dataset = dict(
     type=dataset_type,
-    data_root="/data/ipad/Forgery/test/Columbia/TP/",
+    data_root="/data/ipad/Forgery/pristine/Columbia/TP/",
+    img_suffix = '.tif',
+    seg_map_suffix='.png',
     global_class=1,
     data_prefix=dict(
         img_path="image",
@@ -217,18 +221,22 @@ Columbia_all_test_dataset=dict(
 
 #COVERAGE
 
-COVERAGE_authentic_test_dataset=dict(
+COVERAGE_authentic_test_dataset = dict(
     type=dataset_type,
-    data_root="/data/ipad/Forgery/test/COVERAGE/AU/",
+    data_root="/data/ipad/Forgery/pristine/COVERAGE/AU",
+    img_suffix = '.tif',
+    seg_map_suffix='.png',
     global_class=0,
     data_prefix=dict(
         img_path="image",
         seg_map_path="GT"),
     pipeline=test_pipeline)
 
-COVERAGE_tamp_test_dataset=dict(
+COVERAGE_tamp_test_dataset = dict(
     type=dataset_type,
-    data_root="/data/ipad/Forgery/test/COVERAGE/TP/",
+    data_root="/data/ipad/Forgery/pristine/COVERAGE/TP",
+    img_suffix = '.tif',
+    seg_map_suffix='.tif',
     global_class=1,
     data_prefix=dict(
         img_path="image",
@@ -240,7 +248,7 @@ COVERAGE_all_test_dataset=dict(
     datasets=[COVERAGE_authentic_test_dataset,COVERAGE_tamp_test_dataset])
 
 #NIST16
-NIST16_tamp_test_dataset=dict(
+NIST16_564_tamp_test_dataset=dict(
     type=dataset_type,
     data_root="/data/ipad/Forgery/test/NIST16/",
     global_class=1,
@@ -250,31 +258,124 @@ NIST16_tamp_test_dataset=dict(
         seg_map_path="GT"),
     pipeline=test_pipeline)
 
+NIST16_160_tamp_test_dataset=dict(
+    type=dataset_type,
+    data_root="/data/ipad/Forgery/test/Nist16_subset_160/TP/",
+    global_class=1,
+    # indices=30,
+    data_prefix=dict(
+        img_path="image",
+        seg_map_path="GT"),
+    pipeline=test_pipeline)
+
+NIST16_160_auth_test_dataset=dict(
+    type=dataset_type,
+    data_root="/data/ipad/Forgery/test/Nist16_subset_160/AU/",
+    global_class=0,
+    # indices=30,
+    data_prefix=dict(
+        img_path="image",
+        seg_map_path="GT"),
+    pipeline=test_pipeline)
+
+
+Nist16_160_test_all_dataset=dict(
+    type='ConcatDataset',
+    datasets=[NIST16_160_auth_test_dataset,NIST16_160_tamp_test_dataset])
+
+
+NIST16_tampSP_test_dataset=dict(
+    type=dataset_type,
+    data_root="/data/ipad/Forgery/test/NIST16/",
+    global_class=1,
+    # indices=30,
+    data_prefix=dict(
+        img_path="splice",
+        seg_map_path="GT"),
+    pipeline=test_pipeline)
 #OpenForensics
+# open_au_test_indices=np.random.randint(0, 6670,2000)
+# open_tp_test_indices=np.random.randint(0,9988,2000)
+#config import 会报错 因为lazy import这里的配置文件无法使用import东西的功能
+
 OpenForensics_tamp_test_dataset=dict(
     type=dataset_type,
-    data_root="/data/ipad/Forgery/test/OpenForensics/TP/",
+    data_root="/data/ipad/Forgery/test/OpenForensics_test/TP",
     global_class=1,
     data_prefix=dict(
         img_path="image",
         seg_map_path="GT"),
-    indices=200,
     pipeline=test_pipeline)
 
 OpenForensics_authentic_test_dataset=dict(
     type=dataset_type,
-    data_root="/data/ipad/Forgery/test/OpenForensics/AU/",
+    data_root="/data/ipad/Forgery/test/OpenForensics_test/AU",
     global_class=0,
     data_prefix=dict(
         img_path="image",
         seg_map_path="GT"),
-    indices=100,
     pipeline=test_pipeline)
 
 OpenForensics_all_test_dataset=dict(
     type='ConcatDataset',
     datasets=[OpenForensics_tamp_test_dataset, OpenForensics_authentic_test_dataset])
 
+#CocoGlide
+
+CocoGlide_tamp_test_dataset = dict(
+    type=dataset_type,
+    data_root="/data/ipad/Forgery/pristine/CocoGlide/TP/",
+    img_suffix = '.png',
+    seg_map_suffix='.png',
+    global_class=1,
+    data_prefix=dict(
+        img_path='image',
+        seg_map_path='GT'),
+    # indices=5,
+    pipeline=test_pipeline)
+
+CocoGlide_authentic_test_dataset = dict(
+    type=dataset_type,
+    data_root="/data/ipad/Forgery/pristine/CocoGlide/AU",
+    img_suffix='.png',
+    seg_map_suffix='.png',
+    global_class=0,
+    data_prefix=dict(
+        img_path='image',
+        seg_map_path='GT',
+    ),
+    pipeline=test_pipeline,
+)
+CocoGlide_all_test_dataset=dict(
+    type='ConcatDataset',
+    datasets=[CocoGlide_tamp_test_dataset, CocoGlide_authentic_test_dataset])
+
+#DSO-1
+DSO_1_tamp_test_dataset = dict(
+    type=dataset_type,
+    data_root="/data/ipad/Forgery/pristine/DSO-1/TP/",
+    img_suffix = '.png',
+    seg_map_suffix='.png',
+    global_class=1,
+    data_prefix=dict(
+        img_path='image',
+        seg_map_path='GT'),
+    # indices=5,
+    pipeline=test_pipeline)
+
+DSO_1_authentic_test_dataset=dict(
+    type=dataset_type,
+    data_root="/data/ipad/Forgery/test/DSO-1/AU",
+    global_class=0,
+    data_prefix=dict(
+        img_path='image',
+        seg_map_path='GT',
+    ),
+    pipeline=test_pipeline,
+)
+DSO_1_all_test_dataset=dict(
+    type='ConcatDataset',
+    datasets=[DSO_1_tamp_test_dataset, DSO_1_authentic_test_dataset])
 
 val_dataloader = dict(
     batch_size=1,
@@ -283,9 +384,34 @@ val_dataloader = dict(
     sampler=dict(type='DefaultSampler', shuffle=False),
     dataset=dict(
         type='ConcatDataset',
-        datasets=[Columbia_all_test_dataset])
+        datasets=[
+                  # NIST16_160_tamp_test_dataset,
+                  Columbia_tamp_test_dataset,
+                  # COVERAGE_tamp_test_dataset,
+                  # CASIAv1_plus_tamp_test_dataset,
+                  ])
 
 )
+#IMD2020
+IMD2020_tamp_test_dataset = dict(
+    type=dataset_type,
+    data_root="/data/ipad/Forgery/train/IMD2020/TP/",
+    global_class=1,
+    data_prefix=dict(
+        img_path='image',
+        seg_map_path='GT'),
+    # indices=5,
+    pipeline=test_pipeline)
+
+my_test_dataset = dict(
+    type=dataset_type,
+    data_root='/data/ipad/Forgery/test/NIST16/exp/resizegt/',
+    global_class=1,
+    data_prefix=dict(
+        img_path='image',
+        seg_map_path='GT'),
+    # indices=5,
+    pipeline=test_pipeline)
 
 test_dataloader = dict(
     batch_size=1,
@@ -295,18 +421,36 @@ test_dataloader = dict(
     dataset=dict(
         type='ConcatDataset',
         datasets=[
-            # OpenForensics_all_test_dataset,
-            # CASIAv1_plus_all_test_dataset,
-            # COVERAGE_all_test_dataset
-            Columbia_all_test_dataset,
+            # OpenForensics_tamp_test_dataset,
+
+            # CASIAv1_plus_tamp_test_dataset,
+
+
+            # COVERAGE_tamp_test_dataset,
+
+
+            # Columbia_tamp_test_dataset,
+
+            DSO_1_tamp_test_dataset,
+
+            # NIST16_160_tamp_test_dataset,
+
+
+            # CocoGlide_tamp_test_dataset,
+
+
+            # my_test_dataset
             ])
+
 
 
 )
 
 
 
-val_evaluator=dict(type='IoUMetric', iou_metrics=['mIoU'])
-test_evaluator=dict(type='IoUMetric', iou_metrics=['mIoU'])
+# val_evaluator=dict(type='IoUMetric', iou_metrics=['mIoU'])
+val_evaluator=dict(type='MyFmeasure', iou_metrics=['mFscore']) #调参
+test_evaluator=dict(type='MyFmeasure', iou_metrics=['mFscore'])
+# test_evaluator=dict(type='MyIoUMetric', iou_metrics=['myFscore'])
 
 
